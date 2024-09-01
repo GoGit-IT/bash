f707f6e23ca53eef287c569d0e2801eee8624b9c
Mato <140983012+GoGit-IT@users.noreply.github.com>
2024-09-01 14:51:37 +0200

    Add files via upload

---
+++ bashtest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+clear
+sleep 1
+echo "Loading."
+sleep 1
+clear
+echo "Loading.."
+sleep 1
+clear
+echo "Loading..."
+sleep 1
+clear
+echo "Done!"
+echo "Wellcome $(whoami)!"
+echo "This $(date '+%A') you ran a test bash script successfully!"
