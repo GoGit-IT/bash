f7e56fe736622ce6b6b35fbd00e595267ddd3876
Mato <140983012+GoGit-IT@users.noreply.github.com>
2024-09-01 14:38:19 +0200

    Update whoami.sh
    Extended running period

--- whoami.sh
+++ whoami.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-echo "hello, today is $(date '+%A'). You are running me as $(whoami)."
+clear
+echo "Loading."
+sleep .9
+clear
+echo "Loading.."
+sleep .8
+clear
+echo "Loading..."
+sleep .7
+clear
+echo "Done!"
+echo "Wellcome $(whoami)!"
+echo "Today is $(date '+%A') and you ran this bash test script successfully!"
